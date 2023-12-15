504a955b95cba1368036a7987dc4cb3c90c825c8
STUDENTS-1211-3 <STUDENTS-1211-3@DESKTOP-PMVUDLD>
2023-12-15 11:26:39 +0200

    Add project files.

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи броя на числата: ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Въведи число {i}: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 100)
+                {
+                    sum += num;
+                }
+            }
+
+            if (sum < 100)
+            {
+                Console.WriteLine($"Сумата на числата е под 100:" +sum);
+            }
+            else
+            {
+                Console.WriteLine($"Сумата на числата е равна на:" +sum);
+            }
+        }
+    }
+}
